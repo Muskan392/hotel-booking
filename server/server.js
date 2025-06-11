a597a2dfa278dadbc87f66fdf6c3b1ce1c3d19c8
Muskan Keshri <keshrimuskan997@gmail.com>
2025-06-11 17:31:45 +0530

    webhook error fixed

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
 import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
 import clerkwebhooks from './controller/clerkWebhooks.js';
 
 connectDB();
@@ -10,15 +9,13 @@ connectDB();
 const app = express();
 app.use(cors());
 
-app.use(express.json());
-app.use(clerkMiddleware());
+app.post("/api/clerk", express.raw({ type: 'application/json' }),clerkwebhooks)
 
-//API to handle Clerk webhooks
-app.use("/api/clerk",clerkwebhooks)
+
+app.use(express.json());
 
 app.get('/', (req, res) => res.send('API is working fine!'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = 8080;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-   
